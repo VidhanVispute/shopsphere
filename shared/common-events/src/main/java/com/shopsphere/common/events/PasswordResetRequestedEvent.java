311d49307887e808bfc278301bf018ec922523fa
Vidhan Vispute <vispute.vidhan.it@gmail.com>
2026-03-08 17:46:30 +0530

    fix(common-events): add UserRegisteredEvent and PasswordResetRequestedEvent

---
+++ shared/common-events/src/main/java/com/shopsphere/common/events/PasswordResetRequestedEvent.java
@@ -0,0 +1,20 @@
+package com.shopsphere.common.events;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PasswordResetRequestedEvent {
+    private UUID userId;
+    private String email;
+    private String resetToken;
+    private LocalDateTime expiresAt;
+}
